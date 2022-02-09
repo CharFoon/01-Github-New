ee9c044688a9894905ee4f8dfb38820de003f89f
CharFoon <designerforlife98@gmail.com>
2022-02-08 21:06:26 -0500

    Completed

--- 19.2-activities/03_todo_app/unsolved/js/index.js
+++ 19.2-activities/03_todo_app/unsolved/js/index.js
@@ -1,5 +1,12 @@
 // Create event listener to enter items below
+$("#enter").on("click", function() {
+    var task = $("#todoItem").val();
+    $("#todoList").append("<div class="inputContainer">
+    <input type="input" id="todoItem">
+    <button id="enter"><i class="fas fa-pencil-alt"></i></button>
+  </div>")
 
+}
 // Create event listener to delete items below
 
 // Create event listener to toggle 
